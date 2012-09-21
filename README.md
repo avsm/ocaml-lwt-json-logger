e3315dad52a10a61bee516d556205c8066dc176e
Anil Madhavapeddy <anil@recoil.org>
2012-09-21 16:53:52 -0400

    fix up ocamldoc and install docs by default

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 This library provides an Lwt logger which opens an HTTP listening port and
-serves up log messages as JSON. It also includes a simple Bootstrap+jQuery HTML
-page which regularly polls the JSON port on the server and renders them to a
-web browser.
+serves up log messages as JSON. 
+
+It bundles an embedded Bootstrap+jQuery HTML page which regularly polls the
+JSON port on the server and renders them to a web browser.
 
 To try it out, just run 'make test' and connect to http://localhost:8080
-immediately.
-
+immediately to see the live graphs.
--- lib/lwt_json_logger.mli
+++ lib/lwt_json_logger.mli
@@ -14,22 +14,22 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *)
 
-(** 
-  * Make a JSON logger and HTTP server to serve up the pages.
-  * @param address The IP address to listen on (default 127.0.0.1)
-  * @param port HTTP TCP port to listen on
-  *)
 val make : ?address:string -> port:int -> unit -> Lwt_log.logger
+(** 
+  Make a JSON logger and HTTP server to serve up the pages.
+  @param address The IP address to listen on (default 127.0.0.1)
+  @param port HTTP TCP port to listen on
+ *)
 
-(**
-  * Log a progress bar to the HTTP interface. Takes a "task" to identify
-  * the specific bar, and an Lwt_stream to push the client pushes width
-  * updates (where the width is a percentage (from 0-100).
-  * This actually leaks a bit of memory for each progress bar at present,
-  * since it uses a fake Lwt_log.Section level to mark the update as a 
-  * progress bar. Requires per-log metadata to anything better I think.
-  *)
 val progress :
   task:string ->
   stream:float Lwt_stream.t ->
   logger:Lwt_log.logger -> level:Lwt_log.level -> unit Lwt.t
+(**
+  Log a progress bar to the HTTP interface. Takes a [task] to identify
+  the specific bar, and an Lwt_stream to push the client pushes width
+  updates (where the width is a percentage (from 0-100).
+  This actually leaks a bit of memory for each progress bar at present,
+  since it uses a fake Lwt_log.Section level to mark the update as a 
+  progress bar. Requires per-log metadata to anything better I think.
+ *)
